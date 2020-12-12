140dbb37197aa962755c64d6726c06a792ef517f
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-12-12 20:07:49 +0000

    Java StringReverse, StringsIntro, Substring, SubstringComparisons

---
+++ Java/Strings/src/StringReverse.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class StringReverse {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        System.out.println((isPalindromeRecursive(A) ? "Yes" : "No"));
+        System.out.println((isPalindrome(A) ? "Yes" : "No"));
+    }
+
+    private static boolean isPalindromeRecursive(String s) {
+        if (s.length() <= 1) return true;
+        if (s.charAt(0) == s.charAt(s.length() - 1)) return isPalindromeRecursive(s.substring(1, s.length() - 1));
+        return false;
+    }
+
+    private static boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i++ <= j--) {
+            if (s.charAt(i) != s.charAt(j)) return false;
+        }
+        return true;
+    }
+}
