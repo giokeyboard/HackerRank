7f8d07937241b4a3e5152441d98430827376fd51
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-12-13 15:25:35 +0000

    Java intro - EndOfFile

---
+++ Java/IntroductionNew/EndOfFile/src/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int line = 1;
+        while (sc.hasNext()) {
+            System.out.printf("%d %s%n", line++, sc.nextLine());
+        }
+
+        sc.close();
+    }
+}
