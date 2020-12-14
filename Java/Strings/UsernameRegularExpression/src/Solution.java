a2ceb9c5c0dcd58603af317dbeb33082ebda2d9d
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-12-14 18:20:36 +0000

    Java Strings - UsernameRegEx fixed

--- Java/Strings/UsernameRegularExpression/src/Solution.java
+++ Java/Strings/UsernameRegularExpression/src/Solution.java
@@ -1,9 +1,6 @@
 import java.util.Scanner;
 
 class UsernameValidator {
-    /*
-     * Write regular expression here.
-     */
     public static final String regularExpression = "^[a-zA-Z]\\w{7,29}$";
 }
 
