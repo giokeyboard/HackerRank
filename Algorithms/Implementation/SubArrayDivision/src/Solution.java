51fdcf94d8993040261fa2f68333f385212cdfb7
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-12-15 17:28:33 +0000

    Fix: Algos - SubArrayDivision

--- Algorithms/Implementation/SubArrayDivision/src/Solution.java
+++ Algorithms/Implementation/SubArrayDivision/src/Solution.java
@@ -1,11 +1,22 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Solution {
+    private static final Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println(birthday(Arrays.asList(1, 2, 1, 3, 2), 3, 2));
-        System.out.println(birthday(Arrays.asList(1, 1, 1, 1, 1, 1), 3, 2));
-        System.out.println(birthday(Arrays.asList(4), 4, 1));
+        int n = scan.nextInt();
+        scan.nextLine();
+        List<Integer> s = Arrays.stream(scan.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        int d = scan.nextInt();
+        int m = scan.nextInt();
+        scan.close();
+
+        System.out.println(birthday(s, d, m));
     }
 
     static int birthday(List<Integer> s, int d, int m) {
