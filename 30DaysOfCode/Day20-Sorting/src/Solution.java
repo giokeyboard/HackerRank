8851ad0ba17ecc491a7ac216fef64f61c37ae10d
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2021-01-06 18:27:15 +0000

    30DaysOfCode-day20 improved

--- 30DaysOfCode/Day20-Sorting/src/Solution.java
+++ 30DaysOfCode/Day20-Sorting/src/Solution.java
@@ -5,8 +5,8 @@ public class Solution {
 
     private static int bubbleSort(int[] arr) {
         int numSwaps = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 1; j++) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
