b6aa65ab31af9a18b45e830faf69df86dee2d104
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2021-01-09 15:45:13 +0000

    30DaysOfCode-day24

---
+++ 30DaysOfCode/Day24-MoreLinkedLists/src/Solution.java
@@ -0,0 +1,70 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+
+}
+
+class Solution {
+
+    public static Node removeDuplicates(Node head) {
+        if (head == null) return null;
+        Set<Integer> map = new HashSet<>();
+        map.add(head.data);
+        Node current = head;
+        while (current.next != null) {
+            if (map.contains(current.next.data)) {
+                current.next = current.next.next;
+            } else {
+                map.add(current.next.data);
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    public static Node insert(Node head, int data) {
+        Node p = new Node(data);
+        if (head == null)
+            head = p;
+        else if (head.next == null)
+            head.next = p;
+        else {
+            Node start = head;
+            while (start.next != null)
+                start = start.next;
+            start.next = p;
+
+        }
+        return head;
+    }
+
+    public static void display(Node head) {
+        Node start = head;
+        while (start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head, ele);
+        }
+        head = removeDuplicates(head);
+        display(head);
+
+    }
+}
