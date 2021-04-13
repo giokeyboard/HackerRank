db31a5ce0575c04d95df90dd049d7d7fc1a39f5f
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2021-04-13 14:49:08 +0100

    C: Conditionals and Loops

---
+++ C/ConditionalsAndLoops/patterns.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int n, i, j, len, dist;
+
+    if (scanf("%d", &n) != 1)
+        exit(EXIT_FAILURE);
+
+    len = 2 * n - 1;
+
+    for (i = 0; i < len; i++)
+    {
+        for (j = 0; j < len; j++)
+        {
+            dist = (i < j) ? i : j;
+            dist = (dist < len - i) ? dist : len - i - 1;
+            dist = (dist < len - j - 1) ? dist : len - j - 1;
+            printf("%d ", n - dist);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
